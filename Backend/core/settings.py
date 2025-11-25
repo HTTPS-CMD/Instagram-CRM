49021e2fda20e8a854556b0c5f7dfd89a0caadba
Mehrshad <shadmehrbiriya@gmail.com>
2025-11-25 17:33:43 +0330

    setting

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-+3nnd*ge_853xo#3=wpx-14ss@(qn72)q(v(p+kps6gvwpep%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = [ '*']
+# '127.0.0.1', 'localhost',
 
 
 # Application definition
